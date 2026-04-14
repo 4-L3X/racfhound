c9f03cf4ba33ad41320c60b587b92be2caaa2705
Alex H <a.henrixon22@gmail.com>
2026-04-14 08:54:32 +0100

    Borked icons script

--- icons/load_icons.py
+++ icons/load_icons.py
@@ -5,6 +5,30 @@ import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def bloodhound_get_token(url, username, password):
+    login_url = url + "/api/v2/login"
+    payload = {
+            "login_method": "secret",
+            "username": "" % username,
+            "password": "" % password
+    }
+    print(f"Username: {username}, Password: {password}")
+    try:
+        response = requests.post(login_url, json=payload, timeout=30, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+            token = data['data']['session_token']
+            print("Login successful, retrieving JWT token")
+            return token
+        else:
+            print(f"Login failed: {response.status_code}")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Login error: {e}")
+        sys.exit(1)
+
+
+
 def define_icon(url, token, icon_type, icon_name, icon_color):
     payload = {
         "custom_types": {
@@ -37,18 +61,23 @@ def define_icon(url, token, icon_type, icon_name, icon_color):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("[!] Missing arguments.", file=sys.stderr)
-        print("[?] Usage: load-icons.py <bloodhound-url> <jwt-token>", file=sys.stderr)
-        print("[?] Example: load_icons.py http://127.0.0.1:8080 ey[...]", file=sys.stderr)
+        print("[?] Usage: load-icons.py <bloodhound-url> <bloodhound-username> <bloodhound-password>", file=sys.stderr)
+        print("[?] Example: load_icons.py http://127.0.0.1:8080 USERNAME PASSWORD", file=sys.stderr)
         sys.exit(1)
 
     CUSTOM_NODE_ENDPOINT = "/api/v2/custom-nodes"
-    url = sys.argv[1] + CUSTOM_NODE_ENDPOINT
-    token = sys.argv[2]
+    url = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
+
+    token = bloodhound_get_token(url, username, password)
+
+    icon_url = url + CUSTOM_NODE_ENDPOINT
 
     # Call function for each icon type you want to send
-    define_icon(url, token, "RACF_Group", "user-group", "#00FF00")
-    define_icon(url, token, "RACF_User", "user", "#FF0000")
-    define_icon(url, token, "RACF_Resource", "list-ul", "#FFFF00")
-    define_icon(url, token, "RACF_Dataset", "file", "#0000FF")
+    define_icon(icon_url, token, "RACF_Group", "user-group", "#FFFF60")
+    define_icon(icon_url, token, "RACF_User", "user", "#60FF60")
+    define_icon(icon_url, token, "RACF_Resource", "list-ul", "#6060FF")
+    define_icon(icon_url, token, "RACF_Dataset", "file", "#FF6060")
